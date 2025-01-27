005bf9ef5d616e393cf6c2cf9e7b9e453f76558f
DESKTOP-I6ENAU2\Sunil <sunil_karmakar@ymail.com>
2025-01-27 10:18:10 +0600

    Rearrange code.

--- ExamCore/ExamCore.Shared/ErrorMessages/ProvideErrorMessage.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExamCore.Shared.ErrorMessages
-{
-    public static class ProvideErrorMessage
-    {
-        public static readonly string CountryIdNotFound = "Country id cannot found! Please, try again.";
-        public static readonly string CountryNotFound = "Country cannot found! Please, try again.";
-    }
-}
